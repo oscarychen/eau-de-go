ea8ee8f18b624cd962904a1f793bc36e18383b43
Oscar Chen <oscar.y.chen@gmail.com>
2024-01-23 00:10:01 -0700

    Implememnt JWT token operations

---
+++ internal/jwt_auth/error.go
@@ -0,0 +1,15 @@
+package jwt_auth
+
+import "fmt"
+
+type InvalidTokenError struct {
+	token string
+	msg   *string
+}
+
+func (e *InvalidTokenError) Error() string {
+	if e.msg != nil {
+		return fmt.Sprintf("Invalid token: %s", *e.msg)
+	}
+	return "Invalid token."
+}
---
+++ internal/jwt_auth/token.go
@@ -0,0 +1,89 @@
+package jwt_auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+type TokenType string
+
+const (
+	Refresh TokenType = "refresh"
+	Access  TokenType = "access"
+)
+
+func addStandardClaims(claims *map[string]interface{}, tokenLife time.Duration) {
+	if *claims == nil {
+		*claims = make(map[string]interface{})
+	}
+
+	now := time.Now()
+	(*claims)["iat"] = now.Unix()
+	(*claims)["exp"] = now.Add(tokenLife).Unix()
+	(*claims)["jti"] = uuid.New().String()
+}
+
+func CreateToken(tokenType TokenType, claims ...map[string]interface{}) (string, error) {
+	var tokenClaims map[string]interface{}
+	if len(claims) > 0 {
+		tokenClaims = claims[0]
+	} else {
+		tokenClaims = make(map[string]interface{})
+	}
+	tokenClaims["token_type"] = string(tokenType)
+
+	var tokenLife time.Duration
+	if tokenType == Refresh {
+		tokenLife = time.Hour * 24 * 14
+	} else if tokenType == Access {
+		tokenLife = time.Minute * 15
+	}
+
+	addStandardClaims(&tokenClaims, tokenLife)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodPS256, jwt.MapClaims(tokenClaims))
+
+	signingKey, err := GetInMemoryRsaKeyPair().GetSigningKey()
+	tokenString, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func DecodeToken(tokenType TokenType, tokenString string) (map[string]interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return GetInMemoryRsaKeyPair().GetVerificationKey()
+	})
+
+	if err != nil {
+		msg := err.Error()
+		return nil, &InvalidTokenError{token: tokenString, msg: &msg}
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		if err := validateTokenTypes(claims, tokenType); err != nil {
+			return nil, err
+		}
+		if err := validateJti(claims); err != nil {
+			return nil, err
+		}
+		return claims, nil
+	} else {
+		return nil, &InvalidTokenError{token: tokenString}
+	}
+}
+
+func validateTokenTypes(claims map[string]interface{}, tokenType TokenType) error {
+	if claims["token_type"] != string(tokenType) {
+		msg := "Invalid token type."
+		return &InvalidTokenError{msg: &msg}
+	}
+	return nil
+}
+
+func validateJti(claims map[string]interface{}) error {
+	return nil
+}
