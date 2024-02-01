e4133b6308933664b919616c8adcaae52a14727c
Oscar Chen <oscar.y.chen@gmail.com>
2024-01-31 17:10:30 -0700

    Implement token refresh API

--- internal/transport/http/app_user.go
+++ internal/transport/http/app_user.go
@@ -21,7 +21,11 @@ type AppUserService interface {
 	GetAppUserById(ctx context.Context, ID uuid.UUID) (repository.AppUser, error)
 	CreateAppUser(ctx context.Context, appUser repository.CreateAppUserParams) (repository.AppUser, error)
 	GetAppUserTokens(appUser repository.AppUser) (string, map[string]interface{}, string, map[string]interface{}, error)
+	RefreshToken(ctx context.Context, refreshToken string) (string, map[string]interface{}, error)
 }
+
+var refreshTokenCookieName = "refresh"
+var refreshTokenCookiePath = "/api/user/token-refresh"
 
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 
@@ -46,10 +50,10 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	refreshToken, refreshTokenClaims, accessToken, _, err := h.AppUserService.GetAppUserTokens(userDao)
 
 	cookie := http.Cookie{
-		Name:     "refresh",
+		Name:     refreshTokenCookieName,
 		Value:    refreshToken,
 		HttpOnly: true,
-		Path:     "/api/user/refresh",
+		Path:     refreshTokenCookiePath,
 		SameSite: http.SameSiteStrictMode,
 		Secure:   settings.RefreshCookieSecure,
 		Expires:  time.Unix(refreshTokenClaims["exp"].(int64), 0),
@@ -74,7 +78,52 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 		log.Errorf("Error writing response: %v", err)
 		return
 	}
+}
 
+func (h *Handler) TokenRefresh(w http.ResponseWriter, r *http.Request) {
+	refreshTokenCookie, err := r.Cookie(refreshTokenCookieName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	refreshToken := refreshTokenCookie.Value
+	accessToken, accessTokenClaims, err := h.AppUserService.RefreshToken(r.Context(), refreshToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	userId, err := uuid.Parse(accessTokenClaims["id"].(string))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	userDao, err := h.AppUserService.GetAppUserById(r.Context(), userId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	userDto := response.ConvertDbRow(userDao)
+	responseData := response.AppUserLoginResponse{
+		AppUserDto:  userDto,
+		AccessToken: accessToken,
+	}
+
+	jsonData, err := json.Marshal(responseData)
+	if err != nil {
+		log.Errorf("Error marshalling json: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = w.Write(jsonData)
+	if err != nil {
+		log.Errorf("Error writing response: %v", err)
+		return
+	}
 }
 
 func (h *Handler) GetAppUserById(w http.ResponseWriter, r *http.Request) {
