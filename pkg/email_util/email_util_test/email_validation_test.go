48011d4904782548b6f4e64ebcd187070da4b050
Oscar Chen <oscar.y.chen@gmail.com>
2024-02-04 23:56:08 -0700

    Add email address validation

---
+++ pkg/email_util/email_util_test/email_validation_test.go
@@ -0,0 +1,25 @@
+package email_util_test
+
+import (
+	"eau-de-go/pkg/email_util"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidEmail_HappyPath(t *testing.T) {
+	email := "test@example.com"
+	_, err := email_util.ValidateEmail(email)
+	assert.Nil(t, err)
+}
+
+func TestValidEmail_InvalidFormat(t *testing.T) {
+	email := "invalid email"
+	_, err := email_util.ValidateEmail(email)
+	assert.NotNil(t, err)
+}
+
+func TestValidEmail_EmptyString(t *testing.T) {
+	email := ""
+	_, err := email_util.ValidateEmail(email)
+	assert.NotNil(t, err)
+}
